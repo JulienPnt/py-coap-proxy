ae2d1b38a4b4d6a1de55313db59838d1a32c9cde
Julien Pnt <julien99pornet@gmail.com>
2025-06-08 10:48:13 +0200

    [py-toml] Refacto: current to newer version
    [__init__.py] Add: enable access to utils content

--- src/py_coap_proxy/__init__.py
+++ src/py_coap_proxy/__init__.py
@@ -1,4 +1,22 @@
-# py_coap_proxy/__init__.py
+"""
+Package py_coap_proxy
 
-from .coap_proxy import CoAPProxy  # Expose CoAPProxy
-from .coap_proxy_tester import CoAPProxyTester  # Expose CoAPProxyTester
+"""
+from .utils.coap_block_options import *
+from .utils.coap_options import *
+from .utils.constants import *
+from .utils.logs import *
+from .utils.network_utils import *
+from .coap_proxy import *
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "coap_block_options",
+    "coap_options",
+    "constants",
+    "logs",
+    "network_utils",
+    "CoAPProxy",
+    # "TestCoAP",
+]
